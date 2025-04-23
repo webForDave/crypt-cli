bc298bcac04aa361a60c017573d53fefc92c45c3
David Akinola <dakinola54@gmail.com>
2025-04-23 19:28:16 +0100

    glow up

--- encrypt.py
+++ encrypt.py
@@ -1,12 +1,16 @@
 import sys
 from alphabet import alphabet
 
+exit_string = "\nProgram interupted with the keyboard\n"
+
 def get_key():
     try:
         print("\nEnter encryption key (number)")
         user_input = int(input("> "))
 
         if user_input:
+            if user_input == 0:
+                sys.exit("Zero isn't valid")
             if user_input< 0:
                 sys.exit("\nKey must be a positive integer\n")
             else:
@@ -16,25 +20,22 @@ def get_key():
     except ValueError:
         sys.exit("\nInvalid Key, must be an integer\n")
     except KeyboardInterrupt:
-        sys.exit("\nProgram interupted with the keyboard\n")
+        sys.exit(exit_string)
 
 
 def get_message():
     try:
         print("\nEnter your message")
-        user_input = input("> \n").strip()
+        user_input = input("> ").strip()
 
         if user_input:
-            if any(char.isdigit() for char in user_input):
-                sys.exit("\nMessage can only contain an alphabet/letters\n")
-            else:
-                return user_input
+            return user_input
         else:
             sys.exit("\nYou need to input a message\n")
     except ValueError:
         sys.exit("\nMessage cannot be a number\n") 
     except KeyboardInterrupt:
-        sys.exit("\nProgram interupted with the keyboard\n")
+        sys.exit(exit_string)
 
 
 def message_and_key():
@@ -44,13 +45,8 @@ def message_and_key():
             key = get_key()
             if message and key:
                 return [message, key]
-            else:
-                return "Invalid"
-        else:
-            return "Invalid"
-
     except KeyboardInterrupt:
-        sys.exit("\nProgram interupted with the keyboard\n")
+        sys.exit(exit_string)
 
 
 def encrypt():
