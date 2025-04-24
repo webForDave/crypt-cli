5afea1fb20be7a8fed85ce3beff13604745e1c9f
David Akinola <dakinola54@gmail.com>
2025-04-24 08:09:13 +0100

    decrypt feature

--- decrypt.py
+++ decrypt.py
@@ -1,2 +1,23 @@
+from input import message_and_key
+from alphabet import alphabet
+
 def decrypt():
-    pass
+    user = message_and_key()
+    message = user[0]
+    key = user[1]
+    index_store = []
+    new_string = []
+
+    for char in message:
+        if char not in alphabet:
+            index_store.append(char)
+        else:
+            index_store.append(alphabet.index(char) - key)
+
+
+    for char in index_store:
+        if type(char) == str:
+            new_string.append(char)
+        else:
+            new_string.append(alphabet[char % -len(alphabet)])
+    return f"\nYour decrypted message:\n{''.join(new_string)}"
